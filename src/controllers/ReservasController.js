83da165b190415fd17b545b521e143e6cfbd92e1
sircagev <cf.giron04@gmail.com>
2024-03-08 09:42:20 -0500

    Correccion de errores - Registrar Reserva

--- src/controllers/ReservasController.js
+++ src/controllers/ReservasController.js
@@ -4,7 +4,8 @@ import { pool } from '../database/conexion.js'
 export const RegistrarReserva = async (req, res) => {
     try {
 
-        let message;
+        let messageReserva;
+        let messageDetalle;
         let status;
         let { estado_reserva, fk_usuario, detalles } = req.body;
 
@@ -17,34 +18,34 @@ export const RegistrarReserva = async (req, res) => {
         let [ReservaRows] = await pool.query(sqlReserva, valuesReserva);
 
         if (ReservaRows.affectedRows > 0) {
-            message = 'Se registró con éxito la reserva'
+            messageReserva = 'Se registró con éxito la reserva'
             status = 200
         } else {
             return res.status(403).json({ message: "Reserva no registrada" });
         }
 
-        //Obtener ID de la reserva recien creada
+        //Obtener ID de la reserva recién creada
         const fk_reserva = ReservaRows.insertId;
 
         //Insertar detalles de la reserva
         for (const detalle of detalles) {
             //Traer informacion de los detalles
-            const { fk_elemento, cantidad_elemento, observacion } = detalle;
-
+            const { fk_elemento, cantidad_elemento, observacion, estatus } = detalle;
+            console.log(detalle)
             //Insertar de a un detalle
-            const detalleSql = `INSERT INTO detalle_reservas (fk_reserva, fk_elemento, cantidad_elemento, observacion_reserva) VALUES (?,?,?,?)`;
-            const detalleValues = [fk_reserva, fk_elemento, cantidad_elemento, observacion];
-            let DetalleRows = await pool.query(detalleSql, detalleValues);
+            const detalleSql = `INSERT INTO detalle_reservas (fk_reserva, fk_elemento, cantidad_elemento, observacion_reserva, estatus) VALUES (?,?,?,?,?)`;
+            const detalleValues = [fk_reserva, fk_elemento, cantidad_elemento, observacion, estatus];
+            let [DetalleRows] = await pool.query(detalleSql, detalleValues);
 
             if (DetalleRows.affectedRows > 0) {
-                message = "Se registró con éxito el detalle de la reserva";
+                messageDetalle = "Se registró con éxito el detalle de la reserva";
                 status = 200
             } else {
                 return res.status(403).json({ message: "Detalle de reserva no registrada" });
             }
 
              // Actualizar el stock del elemento
-             const actualizarStockSql = `UPDATE elementos SET disponibilidad_stock = disponibilidad_stock - ? WHERE id_elemento = ?`;
+             const actualizarStockSql = `UPDATE elementos SET disponibilidad_stock = disponibilidad_stock - ? WHERE codigo_elemento = ?`;
              const actualizarStockValues = [cantidad_elemento, fk_elemento];
              await pool.query(actualizarStockSql, actualizarStockValues);
         }
@@ -52,8 +53,9 @@ export const RegistrarReserva = async (req, res) => {
         // Confirmar la transacción
         await pool.query("COMMIT");
 
-        return res.status(status).json({ "message": message });
+        return res.status(status).json({ "message": messageReserva + " " + messageDetalle  });
     } catch (e) {
-        return res.status(500).json({ "message": e.message });
+        await pool.query("ROLLBACK");
+        return res.status(500).json({ "message": e });
     }
 };
